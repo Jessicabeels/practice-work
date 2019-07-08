755418bfdaf346845509fc278471af969d9f2660
Jessica Beeli <beeli@jessicas-mbp.corp.adobe.com>
2019-07-08 16:43:18 -0700

    python setup

---
+++ magento/week-7/python-test/hello.py
@@ -0,0 +1,26 @@
+msg = "Hello World"
+print (msg)
+  
+
+for x in range(6):
+    print(msg)
+    print('hi')
+
+
+def sayBlah():
+    for x in range(6):
+        print('blah')
+
+sayBlah()
+
+def sayToo():
+    x = 3
+   # for x in range(1,6):
+    if x > 2:
+        print('tooop')
+
+
+sayToo()
+
+if  5 > 2: 
+    print("5 is greater than 2")
---
+++ magento/week-7/w3-syntax/w3-syntax.py
@@ -0,0 +1,14 @@
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+#https://www.w3schools.com/python/exercise.asp?filename=exercise_sets5
