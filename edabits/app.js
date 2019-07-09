626680fe3bedde785bdf77dd85a2d1221d7166f8
Jessica Beeli <beeli@Jessicas-MacBook-Pro.local>
2019-07-09 16:33:16 -0700

    edabits daily

--- edabits/app.js
+++ edabits/app.js
@@ -8,3 +8,28 @@ console.log(subReddit("https://www.reddit.com/r/funny/"))
 
 
 console.log("hello worlds")
+
+const checkPalindrome = (str) => {
+    let arr = str.split('')
+    let rev = arr.reverse()
+    // return rev + " ///" + arr
+    if (rev === arr){
+        return true
+    } else {
+        return false
+    }
+}
+
+
+console.log(checkPalindrome("scarrsysr"))
+
+const palindromeCheck = (str) => {
+    let rev = str.split('').reverse().join('')
+    if (rev === str){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(palindromeCheck('tiuqwethi'))
