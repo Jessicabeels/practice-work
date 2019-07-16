baa90fd74f84cd0f97b7bf553f57e6469046971a
Jessicabeels <jmbeeli@gmail.com>
2019-07-16 15:30:32 -0700

    edabits daily

--- edabits/app.js
+++ edabits/app.js
@@ -34,3 +34,16 @@ const palindromeCheck = (str) => {
 
 console.log(palindromeCheck('tiuqwethi'))
 ///science
+
+const cityFacts = (city) => {
+    return city.name + " has a population of " + city.population + " and is situated in " + city.continent
+}
+
+
+console.log(
+cityFacts({
+    name: "Paris",
+    population: "2,140,526",
+    continent: "Europe"
+  }))
+  
